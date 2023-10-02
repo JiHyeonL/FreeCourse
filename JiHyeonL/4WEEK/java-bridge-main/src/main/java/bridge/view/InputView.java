4532e1a1bfd17ee470e1f4ad6cf98b39df249d51
lee jihyeon <lejinhy@gmail.com>
2023-10-02 19:10:49 +0900

    feat: inputview readBridgeSzie, readMoving, readGameCommand 완성

--- JiHyeonL/4WEEK/java-bridge-main/src/main/java/bridge/view/InputView.java
+++ JiHyeonL/4WEEK/java-bridge-main/src/main/java/bridge/view/InputView.java
@@ -1,28 +1,41 @@
 package bridge.view;
 
+import bridge.util.PrintMessage;
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    public static PrintMessage printMessage;
 
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public String readBridgeSize() {
+        printMessage.selectBridgeLength();
+
+        String input = Console.readLine();
+        return input;
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        printMessage.selectMoveUpOrDown();
+
+        String input = Console.readLine();
+        return input;
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        printMessage.requestRestartGame();
+
+        String input = Console.readLine();
+        return input;
     }
 }
