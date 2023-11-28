9c23c62932d7e70b75904367a3e4588941dca8fb
lee jihyeon <lejinhy@gmail.com>
2023-11-28 15:09:48 +0900

    chore: 4주차 정리

--- JiHyeonL/4WEEK/java-bridge-main/src/main/java/bridge/controller/BridgeController.java
+++ JiHyeonL/4WEEK/java-bridge-main/src/main/java/bridge/controller/BridgeController.java
@@ -1,12 +1,23 @@
 package bridge.controller;
 
+import bridge.util.PrintMessage;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class BridgeController {
     private InputView inputView;
+    private PrintMessage printMessage;
     private OutputView outputView;
+    private int length;
+    private int direction;
     public void start() {
+        // 시작 메세지
+        printMessage.startGame();
+        // 다리 길이 입력 받기
         inputView.readBridgeSize();
+        // 이동할 칸 선택
+        inputView.readMoving();
+        //
+
     }
 }
